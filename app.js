0656c30f94ceb540d8d4e97bd526871de5cb42d9
Fernando Medrano <fmedrano1998@gmail.com>
2022-05-12 17:26:02 -0500

    Simplify CSS / Replace alerts with appended paragraphs

--- app.js
+++ app.js
@@ -1,32 +1,56 @@
 const catAge = document.getElementById("age");
 const catName = document.getElementById("name");
 const submit = document.getElementById("submit");
+const result = document.getElementById("result");
 
 submit.onclick = function(){
+
     if (catName.value.length != 0) {
-        calculate();
+
+        if (catAge.value.length != 0 && catAge.value >= 0) {
+            calculate();
+        }
+
+        else {
+            alert("Invalid age. Please enter a vaild age")
+        }
     }
+
     else {
-        alert("Name field empty. Please enter a name.");
+        alert("Invalid name. Please enter a valid name.");
     }
 }
 
 function calculate(){
 
+    let catMsg;
+
+    if (result.hasChildNodes()) {
+        result.removeChild(result.firstChild);
+    }
+
     if (catAge.value == 0) {
-        alert(catName.value + " is 0 years old in human years! Who wudda thunk.")
+        catMsg = document.createElement('p');
+        catMsg.innerHTML = catName.value + " is 0 years old in human years! Who wudda thunk.";
+        result.appendChild(catMsg);
     }
 
     else if (catAge.value == 1) {
-        alert(catName.value + " is 15 years old in human years! God help you..")
+        catMsg = document.createElement('p');
+        catMsg.innerHTML = catName.value + " is 15 years old in human years! God help you..";
+        result.appendChild(catMsg);
     }
 
     else if (catAge.value == 2) {
-        alert(catName.value + " is 24 years old in human years! He's all grown up!")
+        catMsg = document.createElement('p');
+        catMsg.innerHTML = catName.value + " is 24 years old in human years! They're all grown up!";
+        result.appendChild(catMsg);
     }
 
     else {
         let humanAge = 24 + ((catAge.value-2) * 4);
-        alert(catName.value + " is " + humanAge + " years old!")
+        catMsg = document.createElement('p');
+        catMsg.innerHTML = catName.value + " is " + humanAge + " years old! Awesome!";
+        result.appendChild(catMsg);
     }
 }
